9ce0a6d2b5657778a0b67496cd7ac86f058451d4
collins lenjo <collinslenjo.cl@gmail.com>
2018-01-27 01:08:30 +0300

    isset

--- server/register.php
+++ server/register.php
@@ -1,6 +1,11 @@
 <?php
+require_once('db.php');
 // register activity
 
+$username = $_POST['username'];
+$email = $_POST['email'];
+$phoneNumber = $_POST['phoneNumber'];
+$password = $_POST['password'];
 
 $stmt = $db_conn->prepare("INSERT INTO users(username,email,phoneNumber,password) VALUES(:username,:email,:phoneNumber,:password)");
 $stmt->execute(array(':username' => $username, ':email' => $email, ':phoneNumber' => $phoneNumber, ':password' => md5($password) ));
