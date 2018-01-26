b99e0a6a719700280747ae08e10c23d4d26c2579
collins lenjo <collinslenjo.cl@gmail.com>
2018-01-27 01:02:53 +0300

    saved

--- server/db.php
+++ server/db.php
@@ -1,2 +1,3 @@
 <?php
 // database connection
+$db_conn = new PDO("mysql:host=localhost;dbname=android;charset=utf8","username","password");
--- server/register.php
+++ server/register.php
@@ -1,2 +1,6 @@
 <?php
 // register activity
+
+
+$stmt = $db_conn->prepare("INSERT INTO users(username,email,phoneNumber,password) VALUES(:username,:email,:phoneNumber,:password)");
+$stmt->execute(array(':username' => $username, ':email' => $email, ':phoneNumber' => $phoneNumber, ':password' => md5($password) ));
