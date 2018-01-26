303b7dd4af3e4c9fabe34049deabc05bd13eda83
collins lenjo <collinslenjo.cl@gmail.com>
2018-01-27 01:25:13 +0300

    Added db.sql

--- server/db.sql
+++ server/db.sql
@@ -1,3 +1,14 @@
 /*
  * SQL File
 */
+
+CREATE TABLE IF NOT EXISTS 'users' (
+	'id' int(11)  NOT NULL AUTO_INCREMENT,
+	'username' VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	'email' VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	'phonenumber' VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	'password' VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	`created_at` timestamp NULL DEFAULT NULL,
+	`updated_at` timestamp NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- server/register.php
+++ server/register.php
@@ -1,7 +1,7 @@
 <?php
+session_start();
 require_once('db.php');
 // register activity
-
 $username = $_POST['username'];
 $email = $_POST['email'];
 $phoneNumber = $_POST['phoneNumber'];
